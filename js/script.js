c7788f19a570bb7c9b5e3e2b1aa8ff3755788a11
IrascibleDog <irascible.dog@gmail.com>
2015-04-06 03:09:05 +0300

    working

--- js/script.js
+++ js/script.js
@@ -23,7 +23,19 @@ $(document).ready(function(){
             $(".social-block").css("margin-left","15px");
         }
     );
+    var count=1;
     $("#show-head-mid").click(function(){
-            $('.show-head').slideToggle(300);
+        $('.show-head').slideToggle(300);
+        if (count== 1){
+            $('#page').css('margin-top','700px');
+            count=0;
+        }
+        else{
+            setTimeout(function(){
+                $('#page').css('margin-top','15px');
+
+                },400);
+            count=1;
+        }
     });
 });
